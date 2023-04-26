bf04a96ecaf0c05890f0c7809594fd8f906cc5de
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-04-26 09:49:56 +0200

    DataTemplateÜbung

---
+++ Personendatenbank/ColorsConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Personendatenbank
+{
+    public class ColorsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            PropertyInfo[] props = (parameter as ObjectDataProvider)?.Data as PropertyInfo[];
+
+            Color color = (Color)value;
+
+            for (int i = 0; i < props?.Length; i++)
+            {
+                var color2 = props[i]?.GetGetMethod()?.Invoke(new object(), new object[] { });
+                if (color.Equals(color2))
+                    return i;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((parameter as ObjectDataProvider)?.Data as PropertyInfo[])?[(int)value]?.GetGetMethod()?.Invoke(new object(), new object[] { });
+        }
+    }
+}
