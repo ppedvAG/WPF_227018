1a2b4f737146d5eabde87c62f036a5f2c8a0231b
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-04-26 11:02:13 +0200

    Validation

---
+++ Validation/IPv4Validation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Validation
+{
+    internal class IPv4Validation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (Regex.IsMatch(value.ToString(), @"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"))
+                return ValidationResult.ValidResult;
+            else
+                return new ValidationResult(false, "Bitte gib eine IPv4-Adresse ein.");
+        }
+    }
+
+
+    public class ValidationRuleCollection : Collection<ValidationRule> { }
+
+    public class ValidationExtension : MarkupExtension
+    {
+        private Binding binding;
+
+        public ValidationExtension(Binding binding, ValidationRuleCollection validationRules)
+        {
+            this.binding = binding;
+
+            foreach (var item in validationRules)
+            {
+                binding.ValidationRules.Add(item);
+            }
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return binding.ProvideValue(serviceProvider);
+        }
+    }
+}
---
+++ Validation/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation
+{
+    internal class Person : IDataErrorInfo
+    {
+        private string name;
+
+        public string Name 
+        { 
+            get => name;
+            set
+            {
+                if(value.All(x => Char.IsLetter(x)))
+                    name = value;
+                else
+                    throw new Exception("Bitte gib nur Buchstaben ein");
+            }
+        }
+
+        public int Alter { get; set; }
+
+
+        public string Error => null;
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case(nameof(Alter)):
+                        if (Alter < 0) return "Bitte gib dein wahres Alter an!";
+                        if (Alter > 150) return "Du sollst nicht lügen!";
+                        break;
+                    default:
+                        break;
+                }
+                return null;
+            }
+        }
+    }
+}
